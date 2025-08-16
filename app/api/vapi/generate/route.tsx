1d8e8b22fd8c2fd9394b1fbd1bc7ca768ba57e32
Divyansh <divyanshverma18may@gmail.com>
2025-08-17 00:59:13 +0530

    added gemini

--- app/api/vapi/generate/route.tsx
+++ app/api/vapi/generate/route.tsx
@@ -1,4 +1,16 @@
-`Prepare questions for a job interview.
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+import { db } from "@/firebase/admin";
+import { getRandomInterviewCover } from "@/lib/utils";
+
+export async function POST(request: Request) {
+  const { type, role, level, techstack, amount, userid } = await request.json();
+
+  try {
+    const { text: questions } = await generateText({
+      model: google("gemini-2.0-flash-001"),
+      prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
@@ -10,4 +22,30 @@
         ["Question 1", "Question 2", "Question 3"]
         
         Thank you! <3
-    `;
+    `,
+    });
+
+    const interview = {
+      role: role,
+      type: type,
+      level: level,
+      techstack: techstack.split(","),
+      questions: JSON.parse(questions),
+      userId: userid,
+      finalized: true,
+      coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
+
+    await db.collection("interviews").add(interview);
+
+    return Response.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return Response.json({ success: false, error: error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
+}
